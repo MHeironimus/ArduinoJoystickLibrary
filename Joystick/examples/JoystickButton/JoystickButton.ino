1b395f4a1027357c78666ecf618a2604fec5d2b2
Matthew Heironimus <heironimus@live.com>
2015-11-20 08:03:42 -0600

    Added simple button press example

---
+++ Joystick/examples/JoystickButton/JoystickButton.ino
@@ -0,0 +1,47 @@
+// Simple example application that shows how to read four Arduino
+// digital pins and map them to the USB Joystick library.
+//
+// The digital pins 9, 10, 11, and 12 are grounded when they are pressed.
+//
+// NOTE: This sketch file is for use with Arduino Leonardo and
+//       Arduino Micro only.
+//
+// by Matthew Heironimus
+// 2015-11-20
+//--------------------------------------------------------------------
+
+#include <Joystick.h>
+
+void setup() {
+  // Initialize Button Pins
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
+
+  // Initialize Joystick Library
+  Joystick.begin();
+}
+
+// Constant that maps the phyical pin to the joystick button.
+const int pinToButtonMap = 9;
+
+// Last state of the button
+int lastButtonState[4] = {0,0,0,0};
+
+void loop() {
+
+  // Read pin values
+  for (int index = 0; index < 4; index++)
+  {
+    int currentButtonState = !digitalRead(index + pinToButtonMap);
+    if (currentButtonState != lastButtonState[index])
+    {
+      Joystick.setButton(index, currentButtonState);
+      lastButtonState[index] = currentButtonState;
+    }
+  }
+
+  delay(50);
+}
+
