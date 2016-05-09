52651e58942fa46a2d29e62857b13322b7484fc9
Heironimus <MCH0708@daugherty.com>
2016-05-09 13:57:20 -0500

    Added additional compiler directives to check for the proper version of the Arduino IDE and compatible device.

--- Joystick/Joystick.h
+++ Joystick/Joystick.h
@@ -23,6 +23,14 @@
 
 #include "HID.h"
 
+#if ARDUINO < 10606
+#error The Joystick library requires Arduino IDE 1.6.6 or greater. Please update your IDE.
+#endif
+
+#if !defined(USBCON)
+#error The Joystick library can only be used with a USB MCU (e.g. Arduino Leonardo, Arduino Micro, etc.).
+#endif
+
 #if !defined(_USING_HID)
 
 #warning "Using legacy HID core (non pluggable)"
